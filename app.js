c7dff2a1df07eab9b1d853cdba68602553e68c7c
cwrsiqueira <cwrsiqueira@msn.com>
2021-10-05 00:04:42 -0300

    Projeto Iniciado

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const socket = require('socket.io');
+const cors = require('cors');
+const app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    app.use(cors());
+    next();
+})
+ 
+app.get('/', function (req, res) {
+  res.send('Servidor rodando...');
+});
+ 
+const server = app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080/");
+});
+
+io = socket(server, {cors: {origin: "*"}});
+
+io.on("connection", (socket) => {
+    console.log(socket.io);
+});
