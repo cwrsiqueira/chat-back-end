6c0d86adc9d28dd0db5eb94dc15722fa6f71c8a5
cwrsiqueira <cwrsiqueira@msn.com>
2021-10-05 03:38:52 -0300

    atualização do sistema

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const socket = require('socket.io');
 const cors = require('cors');
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
@@ -22,5 +24,13 @@ const server = app.listen(8080, () => {
 io = socket(server, {cors: {origin: "*"}});
 
 io.on("connection", (socket) => {
-    console.log(socket.io);
+    console.log(socket.id);
+
+    socket.on("sala_conectada", (dados) => {
+        socket.join(dados);
+    });
+
+    socket.on("enviar_mensagem", (dados) => {
+        socket.to(dados.sala).emit("receber_mensagem", dados.conteudo);
+    });
 });
